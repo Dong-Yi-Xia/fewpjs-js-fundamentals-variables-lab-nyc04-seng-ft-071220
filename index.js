3c4e02f801800b2c7858e443505e2a943e1f36cd
Dong Xia <dong7777xia@gmail.com>
2020-09-11 14:38:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Code your solution in this file!
 const companyName = "Scuber";
-let mostProfitableNeighborhood = "Chesea";
+let mostProfitableNeighborhood = "Chelsea";
+const companyCeo = "Susan Smith";
